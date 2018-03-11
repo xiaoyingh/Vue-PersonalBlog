b98a25aa6accb29c6484d07a2f00a7f0528474c0
xiaoyingh <yh188169@foxmail.com>
2018-03-11 21:06:49 +0800

    add 自定义指令and过滤器

--- my-blog/src/components/ShowBlog.vue
+++ my-blog/src/components/ShowBlog.vue
@@ -34,6 +34,41 @@
                 return blog.title.match(this.search);
             })
         }
+    },
+    //局部当前组件 自定义过滤器
+    filters:{
+//      "to-uppercase":function(value){
+//        return value.toUpperCase();
+//      }
+      "toUppercase":function(value){
+        return value.toUpperCase();
+      },
+      "snippet":function(value){
+        return value.slice(0,100) + "...";
+      },
+
+    },
+    //自定义 指令
+    directives:{
+      "rainbow":{
+          bind(el,binding,vnode){
+            el.style.color = "#" + Math.random().toString(16).slice(2,8);
+          }
+      },
+      "theme":{
+        bind(el,binding,vnode){
+          if(binding.value == "wide"){
+            el.style.maxWidth = "1260px";
+          }else if(binding.value == "narrow"){
+            el.style.maxWidth = "560px";
+          }
+
+          if(binding.arg == 'column'){
+            el.style.background = "#6677cc";
+            el.style.padding = '20px';
+          }
+        }
+      },
     }
 
   }
--- my-blog/src/main.js
+++ my-blog/src/main.js
@@ -7,34 +7,34 @@ import App from './App'
 Vue.use(VueResource)
 Vue.config.productionTip = false
 
-//自定义指令 Vue.directive
-Vue.directive('rainbow',{
-  bind(el,binding,vnode){
-   el.style.color = "#" + Math.random().toString(16).slice(2,8);
-  }
-})
-Vue.directive('theme',{
-  bind(el,binding,vnode){
-    if(binding.value == "wide"){
-      el.style.maxWidth = "1260px";
-    }else if(binding.value == "narrow"){
-      el.style.maxWidth = "560px";
-    }
-
-    if(binding.arg == 'column'){
-        el.style.background = "#6677cc";
-        el.style.padding = '20px';
-    }
-  }
-})
+//全局     自定义指令 Vue.directive
+// Vue.directive('rainbow',{
+//   bind(el,binding,vnode){
+//    el.style.color = "#" + Math.random().toString(16).slice(2,8);
+//   }
+// })
+// Vue.directive('theme',{
+//   bind(el,binding,vnode){
+//     if(binding.value == "wide"){
+//       el.style.maxWidth = "1260px";
+//     }else if(binding.value == "narrow"){
+//       el.style.maxWidth = "560px";
+//     }
+//
+//     if(binding.arg == 'column'){
+//         el.style.background = "#6677cc";
+//         el.style.padding = '20px';
+//     }
+//   }
+// })
 
 //自定义过滤器  Vue.filter
-Vue.filter("to-uppercase",function(value){
-  return value.toUpperCase();
-})
-Vue.filter("snippet",function(value){
-  return value.slice(0,100) + "...";
-})
+// Vue.filter("to-uppercase",function(value){
+//   return value.toUpperCase();
+// })
+// Vue.filter("snippet",function(value){
+//   return value.slice(0,100) + "...";
+// })
 
 /* eslint-disable no-new */
 new Vue({
